a04bb67159c06f979343cd02dc5934088f1731ec
Chris <cpachomski@gmail.com>
2016-07-06 16:57:03 -0400

    create db nuke

---
+++ db/nuke.rb
@@ -0,0 +1,21 @@
+require 'mysql'
+require_relative 'mysql_config'
+CONFIG = MysqlConfig.new
+
+
+begin 
+	con = Mysql.new CONFIG.host, CONFIG.username, CONFIG.password, 'writers'
+	puts "NUKING....."
+	con.query('DELETE FROM Writers')
+	puts "#{con.affected_rows} ROWS NUKED!"
+	puts "#{con.num_rows} ROWS REMAINING!"
+#Error reporting
+rescue Mysql::Error => e
+	puts e.errno
+	puts e.error
+
+
+#Ensure connection closes
+ensure
+	con.close if con
+end
