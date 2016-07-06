4d71d61966bb8584a6fdba54813a9d40e13ff187
Chris <cpachomski@gmail.com>
2016-07-06 18:28:28 -0400

    fix nuke to drop tables instead of remove rows

--- db/nuke.rb
+++ db/nuke.rb
@@ -6,11 +6,11 @@ CONFIG = MysqlConfig.new
 begin 
 	con = Mysql.new CONFIG.host, CONFIG.username, CONFIG.password, 'mta_subway'
 	puts "NUKING....."
-	con.query('DELETE FROM subway_lines')
-	puts "#{con.affected_rows} ROWS NUKED!"
-	con.query('DELETE FROM subway_entrances')
-	puts "#{con.affected_rows} ROWS NUKES!"
-	
+	con.query('DROP TABLE subway_lines')
+	con.query('DROP TABLE subway_entrances')
+
+	puts('All is lost...')
+
 #Error reporting
 rescue Mysql::Error => e
 	puts e.errno
