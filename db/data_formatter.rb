cba65bdec5a06cd8065dcd4b21d15fee8490aae4
Chris <cpachomski@gmail.com>
2016-07-06 16:50:51 -0400

    create data formatter module

---
+++ db/data_formatter.rb
@@ -0,0 +1,18 @@
+module DataFormatter
+
+	def self.get_name row
+		row[0]
+	end
+
+	def self.get_lat row
+		row[1].partition('(').last.split(' ')[0].to_f
+	end
+
+	def self.get_lng row
+		row[1].partition('(').last.split(' ')[1].to_f
+	end
+
+	def self.get_lines row
+		row[3].split('-')
+	end
+end
