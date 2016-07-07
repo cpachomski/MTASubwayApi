b80b08daba61c7ad1982d6f2559bbd4e1b6e2831
Chris <cpachomski@gmail.com>
2016-07-07 15:48:03 -0400

    add seed again

--- db/seed.rb
+++ db/seed.rb
@@ -30,33 +30,33 @@ begin
 	data_dir =  __dir__ + '/data/subway_entrances.csv'
 
 
-	# #INSERT subway line data
-	# SubwayLines.get_lines.each do |line|
-	# 	con.query("INSERT INTO subway_lines (Name)
-	# 			   VALUES ( '#{line}'  )")
-	# end
+	#INSERT subway line data
+	SubwayLines.get_lines.each do |line|
+		con.query("INSERT INTO subway_lines (Name)
+				   VALUES ( '#{line}'  )")
+	end
 
 
-	# #INSERT subway entrance and line/entrance join data
-	# CSV.foreach(data_dir) do |row|
+	#INSERT subway entrance and line/entrance join data
+	CSV.foreach(data_dir) do |row|
 
-	# 	con.query("INSERT INTO subway_entrances (Name, Lat, Lng)
-	# 			   VALUES( '" + DataFormatter.get_name(row) + "', " + DataFormatter.get_lat(row).to_s + ", " + DataFormatter.get_lng(row).to_s + ");" )
+		con.query("INSERT INTO subway_entrances (Name, Lat, Lng)
+				   VALUES( '" + DataFormatter.get_name(row) + "', " + DataFormatter.get_lat(row).to_s + ", " + DataFormatter.get_lng(row).to_s + ");" )
 
-	# 	DataFormatter.get_lines(row).each do |line|
-	# 		entrance_id = con.query("SELECT LAST_INSERT_ID();").fetch_row
-	# 		line_id = con.query("SELECT Id FROM subway_lines WHERE Name='" + line +"';").fetch_row
-	# 		puts line_id.class
-	# 		puts entrance_id.class
-	# 		p line_id
-	# 		p entrance_id
+		DataFormatter.get_lines(row).each do |line|
+			entrance_id = con.query("SELECT LAST_INSERT_ID();").fetch_row
+			line_id = con.query("SELECT Id FROM subway_lines WHERE Name='" + line +"';").fetch_row
+			puts line_id.class
+			puts entrance_id.class
+			p line_id
+			p entrance_id
 
-	# 		con.query("INSERT INTO subway_entrances_lines (LineId, EntranceId),
-	# 				   VALUES (" + line_id.to_s + ", " + entrance_id.to_s + "); ")
-	# 	end
+			con.query("INSERT INTO subway_entrances_lines (LineId, EntranceId),
+					   VALUES (" + line_id.to_s + ", " + entrance_id.to_s + "); ")
+		end
 
 
-	# end
+	end
 		#remove first row of csv data from table
 		con.query("DELETE FROM subway_entrances WHERE Lat=0")
 
@@ -66,8 +66,8 @@ begin
 
 #Error reporting
 rescue Mysql::Error => e
-	# puts e.errno
-	# puts e.error
+	puts e.errno
+	puts e.error
 
 
 #Ensure connection closes
