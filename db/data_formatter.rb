9f8f54b1151ea7edd75a2bec87e3f0b1a214cb48
Chris <cpachomski@gmail.com>
2016-07-07 11:15:57 -0400

    create gemfile and hide seed data methods for testing on elastic beanstalk

--- db/data_formatter.rb
+++ db/data_formatter.rb
@@ -11,4 +11,8 @@ module DataFormatter
 	def self.get_lng row
 		row[1].partition('(').last.split(' ')[1].to_f
 	end
+
+	def self.get_lines row
+		row[3].split('-')
+	end
 end
--- db/seed.rb
+++ db/seed.rb
@@ -4,11 +4,11 @@ require_relative 'data_formatter'
 require_relative 'data/subway_lines'
 
 require_relative 'mysql_config'
-CONFIG2 = MysqlConfig.new
+CONFIG = MysqlConfig.new
 
 
 begin 
-	con = Mysql.new CONFIG2.host, CONFIG2.username, CONFIG2.password, 'mta_subway'
+	con = Mysql.new CONFIG.host, CONFIG.username, CONFIG.password, 'mta_subway'
 	puts "Now connected to server #{con.get_server_info}"
 
 	con.query("CREATE TABLE IF NOT EXISTS \
@@ -21,32 +21,53 @@ begin
 		subway_lines (Id INT PRIMARY KEY AUTO_INCREMENT, 
 						 Name VARCHAR(255));")
 
-
+	con.query("CREATE TABLE IF NOT EXISTS \
+		subway_entrances_lines (Id INT PRIMARY KEY AUTO_INCREMENT, 
+						 		LineId INT,
+						 		EntranceId VARCHAR(255));")
 
 
 	data_dir =  __dir__ + '/data/subway_entrances.csv'
 
+
+	#INSERT subway line data
 	SubwayLines.get_lines.each do |line|
 		con.query("INSERT INTO subway_lines (Name)
 				   VALUES ( '#{line}'  )")
 	end
 
 
-		#push name data into database
+	#INSERT subway entrance and line/entrance join data
 	CSV.foreach(data_dir) do |row|
-		puts DataFormatter.get_name(row)
+
 		con.query("INSERT INTO subway_entrances (Name, Lat, Lng)
 				   VALUES( '" + DataFormatter.get_name(row) + "', " + DataFormatter.get_lat(row).to_s + ", " + DataFormatter.get_lng(row).to_s + ");" )
+
+		DataFormatter.get_lines(row).each do |line|
+			entrance_id = con.query("SELECT LAST_INSERT_ID();").fetch_row
+			line_id = con.query("SELECT Id FROM subway_lines WHERE Name='" + line +"';").fetch_row
+			puts line_id.class
+			puts entrance_id.class
+			p line_id
+			p entrance_id
+
+			con.query("INSERT INTO subway_entrances_lines (LineId, EntranceId),
+					   VALUES (" + line_id.to_s + ", " + entrance_id.to_s + "); ")
+		end
+
+
 	end
+		#remove first row of csv data from table
 		con.query("DELETE FROM subway_entrances WHERE Lat=0")
+
 
 
 
 
 #Error reporting
 rescue Mysql::Error => e
-	puts e.errno
-	puts e.error
+	# puts e.errno
+	# puts e.error
 
 
 #Ensure connection closes
