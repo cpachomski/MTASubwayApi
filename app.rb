9a6bc495b632a1ce4a63630e6da51d5a4c5562bd
Chris <cpachomski@gmail.com>
2016-07-11 03:33:08 -0400

    change api rest naming and create lines route

--- app.rb
+++ app.rb
@@ -1,14 +1,40 @@
 require 'sinatra'
-require_relative 'fetchers/get'
+
+#import fetchers
+require_relative 'fetchers/entrances'
+require_relative 'fetchers/lines'
 
 
 get '/' do
 	"Hello Squirrel"
 end
 
-get '/entrances' do
+get '/api/mta/subway/entrances' do
 	content_type :json
 
-	Get.all.to_json
+
+	puts params
+	if params['lat'] && params['lng']
+		puts 'has what i need'
+
+		return Get.all_within_radius(params['lat'], params['lng']).to_json
+	end
+
+	return Entrances.get_all.to_json
 end
 
+get '/api/mta/subway/lines' do
+	content_type :json
+
+	return Lines.get_all.to_json
+end
+
+
+
+# get '/api/mta/subwayentrances/:id' do
+# 	content_type :json
+# end
+
+# get '/api/mta/subwayentrances' do
+
+# end
