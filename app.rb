cfbee8d2d50294212ff2baf5d98f4dea4c84e9a4
Chris <cpachomski@gmail.com>
2016-07-11 14:42:24 -0400

    update base route and move api calls to separate files

--- app.rb
+++ app.rb
@@ -1,38 +1,10 @@
 require 'sinatra'
 
-#import fetchers
-require_relative 'fetchers/entrances'
-require_relative 'fetchers/lines'
+#import APIs
+require_relative 'api/entrances'
+require_relative 'api/lines'
 
-
+#docs
 get '/' do
-	"Hello Squirrel"
+	"DOCS WILL GO HERE PLEASE KINDLY WAIT FOR ME TO GET TO IT"
 end
-
-get '/api/mta/subway/entrances' do
-	content_type :json
-
-
-	puts params
-	if params['lat'] || params['lng'] 
-		return Entrances.get_all_within_radius({:lat => params['lat'], :lng => params['lng'], :radius => params['radius']}).to_json
-	end
-
-	return Entrances.get_all.to_json
-end
-
-get '/api/mta/subway/lines' do
-	content_type :json
-
-	return Lines.get_all.to_json
-end
-
-
-
-# get '/api/mta/subwayentrances/:id' do
-# 	content_type :json
-# end
-
-# get '/api/mta/subwayentrances' do
-
-# end
