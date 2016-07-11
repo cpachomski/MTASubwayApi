7573232f37327955b5730db23e3bb9fbd0ab679d
Chris <cpachomski@gmail.com>
2016-07-11 14:06:52 -0400

    update seed to now create many-many association table between entrances and lines

--- db/seed.rb
+++ db/seed.rb
@@ -9,7 +9,7 @@ require_relative 'db_con'
 
 begin 
 	con = DBCON.create
-	
+
 	puts "Now connected to server #{con.get_server_info}"
 
 	con.query("CREATE TABLE IF NOT EXISTS \
@@ -38,32 +38,26 @@ begin
 	end
 
 
-	#INSERT subway entrance and line/entrance join data
+	#INSERT subway entrance
 	CSV.foreach(data_dir) do |row|
 
 		con.query("INSERT INTO subway_entrances (Name, Lat, Lng)
 				   VALUES( '" + DataFormatter.get_name(row) + "', " + DataFormatter.get_lat(row).to_s + ", " + DataFormatter.get_lng(row).to_s + ");" )
 
-		# DataFormatter.get_lines(row).each do |line|
-		# 	entrance_id = con.query("SELECT LAST_INSERT_ID();").fetch_row
-		# 	line_id = con.query("SELECT Id FROM subway_lines WHERE Name='" + line +"';").fetch_row
-		# 	puts line_id.class
-		# 	puts entrance_id.class
-		# 	p line_id
-		# 	p entrance_id
-
-		# 	con.query("INSERT INTO subway_entrances_lines (LineId, EntranceId),
-		# 			   VALUES (" + line_id.to_s + ", " + entrance_id.to_s + "); ")
-		# end
+		#INSERT subway entrance id and each line id for that stop into many-many associate table
+		DataFormatter.get_lines(row).each do |line|
+			entrance_id = con.query("SELECT LAST_INSERT_ID();").fetch_row
+			line_id = con.query("SELECT Id FROM subway_lines WHERE Name='" + line +"';").fetch_row
+			unless line_id == nil
+				con.query("INSERT INTO subway_entrances_lines (LineId, EntranceId) VALUES (" + line_id[0].to_s + ", " + entrance_id[0].to_s + "); ")
+			end
+		end
 
 
 	end
 
 	#remove first row of csv data from table
 	con.query("DELETE FROM subway_entrances WHERE Lat=0")
-
-
-
 
 
 #Error reporting
